16cd05459070ebf67826dc9bc99646f3fbdaa36f
Bruce-Cyusa-Mukama <onefakemail09@gmail.com>
2025-10-24 10:11:02 +0000

    Deploying to gh-pages from @ Bruce-Cyusa-Mukama/bruce-cyusa-mukama.github.io@b8ff278bc0c49b8fec53e18c6f63bfeae3ddd002 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Bruce-Cyusa-Mukama/brucecyusamukama.github.io",
+    "data-repo": "Bruce-Cyusa-Mukama/bruce-cyusa-mukama.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
