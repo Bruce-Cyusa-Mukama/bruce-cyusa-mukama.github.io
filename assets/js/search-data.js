8c401088f22d5a79fd7d00482849738ab5f06e72
Bruce-Cyusa-Mukama <onefakemail09@gmail.com>
2025-10-27 21:29:46 +0000

    Deploying to gh-pages from @ Bruce-Cyusa-Mukama/bruce-cyusa-mukama.github.io@0ce9a9d1fb3a0726850a7680eb57d278ad8e86f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,27 +4,27 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-lectures",
-          title: "lectures",
+          title: "Lectures",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/lectures/";
           },
         },{id: "nav-resume",
-          title: "resume",
+          title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/resume/";
           },
         },{id: "nav-books",
-          title: "books",
+          title: "Books",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
