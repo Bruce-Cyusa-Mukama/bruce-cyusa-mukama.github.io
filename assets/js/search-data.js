6a7e7d7e2c5d3d64147477f3566eee5a34b7fa9c
Bruce-Cyusa-Mukama <onefakemail09@gmail.com>
2025-11-21 21:16:54 +0000

    Deploying to gh-pages from @ Bruce-Cyusa-Mukama/bruce-cyusa-mukama.github.io@9a940b29d161b83961359566047f700453efc397 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,6 +420,21 @@ ninja.data = [{
           description: "Everything is awesome",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
+            },},{id: "projects-robot-learning",
+          title: 'Robot Learning',
+          description: "Everything is awesome",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-trustworthy-ai",
+          title: 'Trustworthy AI',
+          description: "Everything is awesome",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-trustworthy-robot-learning",
+          title: 'Trustworthy Robot Learning',
+          description: "Everything is awesome",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
             },},{
         id: 'social-email',
         title: 'email',
