d2de813ab800eda2d66dfec105c231b2cff1ace9
Bruce-Cyusa-Mukama <onefakemail09@gmail.com>
2025-11-26 15:56:01 +0000

    Deploying to gh-pages from @ Bruce-Cyusa-Mukama/bruce-cyusa-mukama.github.io@1be7bc4a9c02ec88bbe7269502b2a814abae9812 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "A curated list of the robotics research and development projects to which I contributed.",
+          description: "A curated list of the robotics research projects to which I contributed.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
