b538537bbdf9e2b57fb63d80b9141cea2cf467d5
Bruce-Cyusa-Mukama <onefakemail09@gmail.com>
2025-12-03 20:09:38 +0000

    Deploying to gh-pages from @ Bruce-Cyusa-Mukama/bruce-cyusa-mukama.github.io@2731543e72ad1f2fdf6ab1bda9049f6da8f8cc3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "nav-art",
-          title: "Art",
-          description: "A curated list of my digital art projects (songs, paintings, animated clips, etc.).",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/art/";
-          },
         },{id: "nav-books",
           title: "Books",
           description: "A curated list of my books (in progress or publihed).",
