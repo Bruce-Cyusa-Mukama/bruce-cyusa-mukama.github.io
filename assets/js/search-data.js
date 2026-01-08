c49947a0a03844412317a48446f01e36709387c8
Bruce-Cyusa-Mukama <onefakemail09@gmail.com>
2026-01-08 10:14:04 +0000

    Deploying to gh-pages from @ Bruce-Cyusa-Mukama/bruce-cyusa-mukama.github.io@7ad1dffb79785246acf8feca2c5cf0811d97ea0f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,8 +430,8 @@ ninja.data = [{
           description: "A visual analysis and a fingerstyle guitar cover of the song &quot;RUNAWAY&quot; by OneRepublic.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/art_5/";
-            },},{id: "projects-tlmqj",
-          title: 'TLMQJ',
+            },},{id: "projects-toute-la",
+          title: 'Toute la ...',
           description: "A visual analysis and a fingerstyle guitar cover of the song &quot;Toute la musique que j’aime&quot; by Johnny Hallyday.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/art_6/";
